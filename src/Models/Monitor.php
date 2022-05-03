efc89538c4b0f39e8e38a599298dbfa27dd12b5f
mihaly044 <mihaly044@gmail.com>
2022-05-03 11:46:24 +0200

    Added Job model and relation

--- src/Models/Monitor.php
+++ src/Models/Monitor.php
@@ -6,6 +6,7 @@ use Carbon\CarbonInterval;
 use Exception;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
@@ -281,5 +282,9 @@ class Monitor extends Model implements MonitorContract
         }
 
         return ! $this->hasFailed();
+    }
+
+    public function job(): BelongsTo {
+        return $this->belongsTo(Job::class);
     }
 }
