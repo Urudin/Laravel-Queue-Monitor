24979ad8e10d9c3bd6973eb592c87fa3a86055cb
Urudin <52380688+Urudin@users.noreply.github.com>
2024-01-05 15:03:39 +0100

    Deduplicate Retry Functions

--- routes/queue-monitor.php
+++ routes/queue-monitor.php
@@ -6,12 +6,12 @@ Route::get('', romanzipp\QueueMonitor\Controllers\ShowQueueMonitorController::cl
 
 Route::delete('monitors/{monitor}', romanzipp\QueueMonitor\Controllers\DeleteMonitorController::class)->name('queue-monitor::destroy');
 
-Route::patch('monitors/retry/{monitor}', romanzipp\QueueMonitor\Controllers\RetryMonitorController::class)->name('queue-monitor::retry');
+//Route::patch('monitors/retry/{monitor}', romanzipp\QueueMonitor\Controllers\RetryMonitorController::class)->name('queue-monitor::retry');
 
 if (config('queue-monitor.ui.allow_purge')) {
     Route::delete('purge', romanzipp\QueueMonitor\Controllers\PurgeMonitorsController::class)->name('queue-monitor::purge');
 }
 
 if (config('queue-monitor.ui.allow_retry')) {
-    Route::get('retry/{job_id}', romanzipp\QueueMonitor\Controllers\RetryJobsController::class)->name('queue-monitor::retry');
+    Route::patch('monitors/retry/{monitor}', \romanzipp\QueueMonitor\Controllers\RetryMonitorController::class)->name('queue-monitor::retry');
 }
